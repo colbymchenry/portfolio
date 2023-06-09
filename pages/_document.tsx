2c74c0076f891535aa722e268e9d6ec08fcc1cbd
Colby McHenry <me@colbymchenry.com>
2023-06-09 16:11:35 -0500

    add clarity

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,13 +2,20 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en" style={{scrollBehavior:'smooth'}}>
+    <Html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <Head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script type="text/javascript">
+          (function(c,l,a,r,i,t,y){
+            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "hhdesgpjx5");
+        </script>
       </body>
     </Html>
   )
