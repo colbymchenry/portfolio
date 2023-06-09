25916b3d67caa77e9f0d6b37335ce9c5280eaf64
Colby McHenry <me@colbymchenry.com>
2023-06-09 16:13:03 -0500

    clarity fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,13 +9,7 @@ export default function Document() {
       <body>
         <Main />
         <NextScript />
-        <script type="text/javascript">
-          (function(c,l,a,r,i,t,y){
-            c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
-          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-          })(window, document, "clarity", "script", "hhdesgpjx5");
-        </script>
+        <script src="/clarity.js"></script>
       </body>
     </Html>
   )
